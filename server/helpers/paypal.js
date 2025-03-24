9845ce8b0b74aad76665d8d4c030736c49045e43
SSE World <suryakantmaurya2007@gmail.com>
2025-03-24 18:44:33 +0530

    Add PayPal configuration and implement shop routes for products, cart, orders, addresses, reviews, and features

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,5 +1,5 @@
 const paypal = require("paypal-rest-sdk");
 
-paypal.configure({ mode: "", client_id: "", client_secret: "" });
+paypal.configure({ mode: "sandbox", client_id: "", client_secret: "" });
 
 module.exports = paypal;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { mongoURL, port } = require("./helpers/env");
 
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
+
+const commonFeatureRouter = require("./routes/common/feature-routes");
+
 const app = express();
 connectDB(mongoURL);
 
@@ -24,5 +37,20 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
+
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
+
+app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
