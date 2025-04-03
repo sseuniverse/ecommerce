f2a4d0cbbb6f98bfec81255b53f67073b4fca29f
SSE World <suryakantmaurya2007@gmail.com>
2025-04-03 08:49:46 +0530

    Add SCSS files for primary color utility classes and update build process

--- client/script/sass-build.js
+++ client/script/sass-build.js
@@ -1,27 +1,41 @@
-import * as sass from "sass";
-import fs from "fs";
+// import * as sass from "sass";
+// import fs from "fs";
 
-function generateCSS() {
-  sass.compileAsync(`${process.cwd()}/script/main.scss`).then((data) => {
-    fs.writeFileSync(`${process.cwd()}/src/css/output.css`, data.css, (err) => {
-      if (err) {
-        console.error("An error occurred:", err);
-      } else {
-        console.log("File created successfully!");
-      }
-    });
-    // fs.writeFileSync(
-    //   `${process.cwd()}/src/css/main.css.map`,
-    //   data.sourceMap,
-    //   (err) => {
-    //     if (err) {
-    //       console.error("An error occurred:", err);
-    //     } else {
-    //       console.log("Map file created successfully!");
-    //     }
-    //   }
-    // );
-  });
-}
+// function generateCSS() {
+//   sass
+//     .compileAsync(`${process.cwd()}/script/scss/main.scss`)
+//     .then((data) => {
+//       fs.writeFileSync(`${process.cwd()}/src/css/main.css`, data.css, (err) => {
+//         if (err) {
+//           console.error("An error occurred:", err);
+//         } else {
+//           console.log("File created successfully!");
+//         }
+//       });
 
-generateCSS();
+//       if (data.sourceMap) {
+//         // console.log(data.sourceMap)
+//       }
+//     })
+//     .catch((err) => {
+//       console.error("An error occurred during Sass compilation:", err);
+//     });
+// }
+
+// generateCSS();
+
+import { exec } from "child_process";
+
+const command = "sass ./script/scss/main.scss ./src/css/main.css";
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
